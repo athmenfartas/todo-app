79f0c3c019a09fb24f798f1fad014a1b70f2d34b
athmenfartas <athmenfartas@gmail.com>
2026-01-28 15:29:27 +0100

    create the todo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 
+import Todo from "./components/Todo"
 import todos from "./data/todos.json"
 
 
@@ -6,17 +7,25 @@ function App() {
 
   return (
     <>
-    <h3 className="">Hello , Welcome Back !</h3>
+    <div className="w-3xl h-screen m-auto ">
+    <div className=" justify-center items-center h-full">
+      <div className=" mt-auto">
+
+        <ul className="bg-white shadow overflow-hidden sm:rounded-md max-w-sm mx-auto mt-16">
+ <h3 className="flex justify-center">Hello , Welcome Back !</h3>
+
     
      {
-      todos.todos.map((todo) =>(
-        <div key={todo.id}>
-          <h3>{todo.title}</h3>
-          <p>{todo.decription}</p>
-          <p>{todo.periority}</p>
-        </div>
+      todos.map((todo) =>(
+        <Todo key={todo.id} status={todo.status} title={todo.title} periority={todo.periority} description={todo.decription}/>
       ))
      }
+     </ul>
+      </div>
+
+   
+     </div>
+     </div>
     </>
   )
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
 
 
 
-function Todo({status ,title , periority } : {status : string, title : string , periority : string}) {
+function Todo({key , status ,title , periority , description } : {key: number ,status : string, title : string , periority : string , description : string}) {
   return (
-    <div>
-      <h1>{status}</h1>
-      <h1>{periority}</h1>
-      <h1>{title}</h1>
+     
 
-    </div>
+
+
+
+ <li key={key}>
+        <div className="px-4 py-5 sm:px-6">
+            <div className="flex items-center justify-between">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500">{description}</p>
+            </div>
+            <div className="mt-4 flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-500">Status: <span className="text-green-600">{periority}</span></p>
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Edit</a>
+            </div>
+        </div>
+    </li>
+
+
+
   )
 }
+
+
+
+
 
 export default Todo
