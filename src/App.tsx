062647e2858a32c143edbc78a51279c62c9d7c71
athmenfartas <athmenfartas@gmail.com>
2026-01-28 08:35:48 +0100

    initiate project and clean exsited files

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import todos from "./data/todos.json"
+
+
+function App() {
+
+  return (
+    <>
+    <h3>Hello , Welcome Back !</h3>
+    
+     {
+      todos.todos.map((todo) =>(
+        <div key={todo.id}>
+          <h3>{todo.title}</h3>
+          <p>{todo.decription}</p>
+          <p>{todo.periority}</p>
+        </div>
+      ))
+     }
+    </>
+  )
+}
+
+export default App
